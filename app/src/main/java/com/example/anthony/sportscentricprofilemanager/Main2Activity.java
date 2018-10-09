cbab7c3f0eefe71f51c285aeeee0038e3fe224a9
anthonyaoun23 <anthony.aoun1@hotmail.com>
2018-10-09 14:54:32 -0400

    Adding new intent

---
+++ app/src/main/java/com/example/anthony/sportscentricprofilemanager/Main2Activity.java
@@ -0,0 +1,24 @@
+package com.example.anthony.sportscentricprofilemanager;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void setTeamIcon(View view){
+        Intent returnIntent = new Intent();
+        ImageView selectedImage = (ImageView) view;
+        returnIntent.putExtra("imageID", selectedImage.getId());
+        setResult(RESULT_OK, returnIntent);
+        finish();
+    }
+}
--- app/src/main/java/com/example/anthony/sportscentricprofilemanager/MainActivity.java
+++ app/src/main/java/com/example/anthony/sportscentricprofilemanager/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.anthony.sportscentricprofilemanager;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +15,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void OnOpenInGoogleMaps (View view) {
+        EditText teamAddres = (EditText) findViewById(R.id.teamAddressField);
+        Uri gmmIntentUri = Uri.parse("http://maps.google.co.in/maps?q=" + teamAddres.getText());
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
+    }
+
+    public void OnSetAvatarButton(View view) {
+        Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+        startActivityForResult(intent,0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_CANCELED) return;
+        ImageView avatarImage = (ImageView) findViewById(R.id.avatarImage);
+        String drawableName = "ic_logo_00"; //4
+        switch (data.getIntExtra("imageID",R.id.teamid00)) {//Figuring out the correct image
+            case R.id.teamid00:
+                drawableName = "ic_logo_00";
+                break;
+            case R.id.teamid01:
+                drawableName = "ic_logo_01";
+                break;
+            case R.id.teamid02:
+                drawableName = "ic_logo_02";
+                break;
+            case R.id.teamid03:
+                drawableName = "ic_logo_03";
+                break;
+            case R.id.teamid04:
+                drawableName = "ic_logo_04";
+                break;
+            case R.id.teamid05:
+                drawableName = "ic_logo_05";
+                break;
+            default:
+                drawableName = "ic_logo_00";
+                break;
+        } //27
+
+        int resID = getResources().getIdentifier(drawableName, "drawable",  getPackageName());
+        avatarImage.setImageResource(resID);
+    }
+
 }
